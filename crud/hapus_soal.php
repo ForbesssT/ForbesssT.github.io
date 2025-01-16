a938ad9d41bb4e99f6ddce5f03afc617af3e31e8
ForbesssT <155507396+ForbesssT@users.noreply.github.com>
2025-01-16 12:05:38 +0700

    Add files via upload

---
+++ crud/hapus_soal.php
@@ -0,0 +1,17 @@
+<?php
+// hapus_soal.php
+include 'koneksi.php';
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM soal WHERE id = $id";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Soal berhasil dihapus.";
+    echo "<a href='index.php'>Kembali ke daftar soal</a>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
